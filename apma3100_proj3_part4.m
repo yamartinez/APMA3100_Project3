75eab5439b4d6321dd413f1b4064afafdeb0b07a
eglib <pandarzb@gmail.com>
2021-04-30 04:06:59 -0400

    fixed all_sample_means generation

--- apma3100_proj3_part4.m
+++ apma3100_proj3_part4.m
@@ -36,43 +36,50 @@ if run_4_2_3 == 1
    for i = 1:110
        %select random sample - n = 10, 30, 50, 100, 250, 500, 1000
        %fill all_sample_means with sample means based on selections
-       n1 = randperm(10);
+       sample_mean = 0;
+       n1 = randperm(100000, 10);
        for j = 1:10
           sample_mean = sample_mean + aggregate_sample(n1(j));
        end
        sample_mean = sample_mean/10;
        all_sample_means(1,i) = sample_mean;
-       n2 = randperm(30);
+       sample_mean = 0;
+       n2 = randperm(100000, 30);
        for j = 1:30
           sample_mean = sample_mean + aggregate_sample(n2(j));
        end
        sample_mean = sample_mean/30;
        all_sample_means(2,i) = sample_mean;
-       n3 = randperm(50);
+       sample_mean = 0;
+       n3 = randperm(100000, 50);
        for j = 1:50
           sample_mean = sample_mean + aggregate_sample(n3(j));
        end
        sample_mean = sample_mean/50;
        all_sample_means(3,i) = sample_mean;
-       n4 = randperm(100);
+       sample_mean = 0;
+       n4 = randperm(100000, 100);
        for j = 1:100
           sample_mean = sample_mean + aggregate_sample(n4(j));
        end
        sample_mean = sample_mean/100;
        all_sample_means(4,i) = sample_mean;
-       n5 = randperm(250);
+       sample_mean = 0;
+       n5 = randperm(100000, 250);
        for j = 1:250
           sample_mean = sample_mean + aggregate_sample(n5(j));
        end
        sample_mean = sample_mean/250;
        all_sample_means(5,i) = sample_mean;
-       n6 = randperm(500);
+       sample_mean = 0;
+       n6 = randperm(100000, 500);
        for j = 1:500
           sample_mean = sample_mean + aggregate_sample(n6(j));
        end
        sample_mean = sample_mean/500;
        all_sample_means(6,i) = sample_mean;
-       n7 = randperm(1000);
+       sample_mean = 0;
+       n7 = randperm(100000, 1000);
        for j = 1:1000
           sample_mean = sample_mean + aggregate_sample(n7(j));
        end
